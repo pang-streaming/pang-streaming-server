d041a9d58cad9a18a97418db34e48f5be9de2825
Ingrsso <79688235+eunsdev@users.noreply.github.com>
2025-09-18 11:34:49 +0900

    refactor: add session handler

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,8 @@
-use scuffle_rtmp::{
-    ServerSession,
-    session::server::{ServerSessionError, SessionData, SessionHandler},
-};
+use scuffle_rtmp::ServerSession;
 use tokio::{net::TcpListener, stream};
 
-struct Handler;
-
-impl SessionHandler for Handler {
-    async fn on_data(
-        &mut self,
-        stream_id: u32,
-        data: SessionData,
-    ) -> Result<(), ServerSessionError> {
-        Ok(())
-    }
-
-    // When live stream strart
-    async fn on_publish(
-        &mut self,
-        stream_id: u32,
-        app_name: &str,
-        stream_key: &str,
-    ) -> Result<(), ServerSessionError> {
-        println!("stream_key: {}", stream_key);
-        if stream_key == "123" {
-            println!("stream_id: {}", stream_id);
-            println!("app_name: {}", app_name);
-            println!("stream_key: {}", stream_key);
-
-            Ok(())
-        } else {
-            return Err(ServerSessionError::InvalidChunkSize(0));
-        }
-    }
-
-    // Stream ended
-    async fn on_unpublish(&mut self, stream_id: u32) -> Result<(), ServerSessionError> {
-        Ok(())
-    }
-}
+mod session_handler; // Handler 정의 파일
+use session_handler::Handler;
 
 #[tokio::main]
 async fn main() {
