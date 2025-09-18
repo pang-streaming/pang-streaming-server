98143be3c98fca94950f3e9cadee4fc0680e17d9
Ingrsso <79688235+eunsdev@users.noreply.github.com>
2025-09-18 10:59:29 +0900

    feat: add basic rtmp server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,49 @@
+use scuffle_rtmp::{
+    ServerSession,
+    session::server::{ServerSessionError, SessionData, SessionHandler},
+};
 use tokio::net::TcpListener;
 
+struct Handler;
+
+impl SessionHandler for Handler {
+    async fn on_data(
+        &mut self,
+        stream_id: u32,
+        data: SessionData,
+    ) -> Result<(), ServerSessionError> {
+        // Handle incoming video/audio/meta data
+        Ok(())
+    }
+
+    async fn on_publish(
+        &mut self,
+        stream_id: u32,
+        app_name: &str,
+        stream_name: &str,
+    ) -> Result<(), ServerSessionError> {
+        // Handle the publish event
+        Ok(())
+    }
+
+    async fn on_unpublish(&mut self, stream_id: u32) -> Result<(), ServerSessionError> {
+        // Handle the unpublish event
+        Ok(())
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    println!("Listening for connections on port 1935");
-    let listener = TcpListener::bind("0.0.0.0:1935").await?;
+async fn main() {
+    let listener = TcpListener::bind("[::]:1935").await.unwrap();
+    // listening on [::]:1935
 
-    loop {
-        let (stream, connection_info) = listener.accept().await?;
+    while let Ok((stream, addr)) = listener.accept().await {
+        let session = ServerSession::new(stream, Handler);
 
-        println!("Connect on {}", connection_info.ip())
+        tokio::spawn(async move {
+            if let Err(err) = session.run().await {
+                // Handle the session error
+            }
+        });
     }
 }
